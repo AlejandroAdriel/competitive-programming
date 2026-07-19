d3e0a5d8f132d8408578a9827568406812d4bd5f
AlejandroAdriel <alejandroadrielq@gmail.com>
2026-07-18 19:41:53 -0500

    feat: add competitive programming C++ template with standard I/O optimization

---
+++ templates/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define int ll
+ 
+void solve()
+{
+
+}
+ 
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin.exceptions(cin.failbit);
+    
+    int t = 1;
+    cin >> t; 
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
